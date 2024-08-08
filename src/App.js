c10fb35e5cf8a3e16ea51f1b847b22ba9624134a
Mintesn <mketema4@gmail.com>
2024-08-08 23:56:01 +0300

    calling api and display on browser and also some css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import '../src/App.css'
 
 function App() {
+  const [countryName, setCountryName] = useState('');
+  const [result, setResult] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleSearch = async () => {
+    if (countryName === ""){
+      alert("Please Enter Country Name");
+      return;
+    }
+    setError(null);
+    setResult(null);
+
+    try {
+      const res = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
+
+    //  to stop the execution of the current function
+        if (!res.ok) {
+          throw new Error('Country not found');       
+      }
+      const data = await res.json();
+      const country = data[0];
+      console.log(data[0])
+     
+      setResult({
+        name: country.name.common ,
+        flag: country.flags?.svg ,
+        capital: country.capital ? country.capital[0] : 'not found',
+        population: country.population ? country.population.toLocaleString() : 'not found',
+        languages: country.languages ? Object.values(country.languages).join(', ') : 'not found',
+      });
+    }catch (error) {
+      setError(error.message);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ 
+   )
 }
 
 export default App;
